2be78bd4a85266c697c7a2d58c3c491c431640e5
Mr-tsukky <cr0issant.spacedebris@gmail.com>
2026-05-05 00:48:21 +0900

    update: domain-model

--- shared/domain-model/index.ts
+++ shared/domain-model/index.ts
@@ -3,9 +3,10 @@ import { z } from "zod";
 export const reservationSchema = z.object({
   name: z.string().min(1), // 予約者名
   email: z.string().email(), // 予約者メール
-  phone: z.string(), // 予約者電話番号
   slotId: z.string(), // 観劇日時
-  findFrom: z.enum(["X", "Instagram", "Web", "Other"]), // どこから予約したか
+  count: z.number(), // 予約人数
+  findFrom: z.array(z.string()), // どこから予約したか
+  note: z.string(), // 備考
 });
 
 export type ReservationRequest = z.infer<typeof reservationSchema>;
