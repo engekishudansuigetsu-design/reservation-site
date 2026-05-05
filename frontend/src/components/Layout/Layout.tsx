79bc8d1d04bde952f445918a6d90634bfb90e3c9
smiyahira <uniwashi16@icloud.com>
2026-05-05 19:05:42 +0900

    インデント修正

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -3,12 +3,12 @@ import { Header } from './Header';
 import { Box, Container } from '@chakra-ui/react';
 
 export const Layout = (props: { children: React.ReactNode }) => {
-    return (
-        <Box>
-            <Header/>
-            <Container maxW="container.lg" mt={4}>
-                {props.children}
-            </Container>
-        </Box>
-    );
+	return (
+		<Box>
+			<Header />
+			<Container maxW="container.lg" mt={4}>
+				{props.children}
+			</Container>
+		</Box>
+	);
 }
