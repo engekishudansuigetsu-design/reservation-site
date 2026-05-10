48a1a8a0ea280d016f3f132baf941e7f1b0cf3f2
Mr-tsukky <cr0issant.spacedebris@gmail.com>
2026-05-10 16:52:50 +0900

    feat: sharedにAPI error情報追加

---
+++ shared/errors/apiReturnType.ts
@@ -0,0 +1,14 @@
+import { ErrorCode } from "./errorCodes";
+
+export type ApiSuccessResponse<T> = {
+  result: true;
+  data: T;
+};
+
+export type ApiErrorResponse = {
+  result: false;
+  code: ErrorCode;
+  message: string;
+};
+
+export type ApiResponse<T> = ApiSuccessResponse<T> | ApiErrorResponse;
