4fc05d0a5180e3f861dc4ce996e0c0e221e50bfa
smiyahira <uniwashi16@icloud.com>
2026-05-23 23:12:30 +0900

    fix: 開発環境でのURL修正

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/reservation-site/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/reservation-site/" : "/",
   plugins: [react()],
-});
+}));
