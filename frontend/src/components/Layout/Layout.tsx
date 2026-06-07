f77b536c839d7e78b2229c5ace81428048d952f1
Mr-tsukky <cr0issant.spacedebris@gmail.com>
2026-06-07 15:03:30 +0900

    feat: SNS宣伝エリア追加

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Header } from "./Header";
-import { Box, Container, Flex } from "@chakra-ui/react";
+import { Box, Container, Flex, Text } from "@chakra-ui/react";
 import { Footer } from "./Footer";
 import { Fabs } from "./Fabs";
 import { ResponsiveTopFlyer } from "../Flyer/ResponsiveTopFlyer";
@@ -27,6 +27,9 @@ export const Layout = (props: { children: React.ReactNode }) => {
       <Container maxW="container.md" px={6} mt={4} mb={8} zIndex="content">
         {props.children}
       </Container>
+      <Flex bg="accent.500" py={4} mt={10} direction="column">
+        <Text textAlign="center">公式Xにて最新情報や稽古の様子を配信中!</Text>
+      </Flex>
       <Footer />
     </Box>
   );
